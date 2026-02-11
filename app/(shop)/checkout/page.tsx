d09a03e50cabb9605babf99e87c3d086ee3d62ee
idealindiska-gif <idealindiska@gmail.com>
2026-02-12 04:42:00 +0500

    Update page.tsx

--- app/(shop)/checkout/page.tsx
+++ app/(shop)/checkout/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import Link from 'next/link';
 import { Container, Section } from '@/components/craft';
 import { Button } from '@/components/ui/button';
